d59492e0d1be57cde0d4daac75bf8497daa24c84
linuxdocs2025 <linuxdocs2025@gmail.com>
2025-11-11 15:08:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is DevOps Project
+          This is DevOps Project version 01
         </p>
         <a
           className="App-link"
